d53d7308bcffd6b434b57d97eec5700158065827
regiov <renato@cria.org.br>
2020-12-02 11:42:21 -0300

    Compatibility with newer versions of django in url patterns

--- rep/urls.py
+++ rep/urls.py
@@ -1,57 +1,54 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
+
+from app import views
 
 handler404 = 'app.views.handler404'
 handler500 = 'app.views.handler500'
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'rep.views.home', name='home'),
-    # url(r'^rep/', include('rep.foo.urls')),
+urlpatterns = [
     # Main page:
-    url(r'^$', 'app.views.index'),
+    url(r'^$', views.index),
     # Translation facility
     url(r'^i18n/', include('django.conf.urls.i18n')),
     # Static content page
-    url(r'^p/(?P<page_code>[-\w\d]+)/$', 'app.views.show_page'),
+    url(r'^p/(?P<page_code>[-\w\d]+)/$', views.show_page),
     # Browse species
-    url(r'^sp/?$', 'app.views.search_species'),
+    url(r'^sp/?$', views.search_species),
     # Species page
-    url(r'^sp/(?P<species_id>\d+)/?$', 'app.views.show_species'),
+    url(r'^sp/(?P<species_id>\d+)/?$', views.show_species),
     # "Static" pages
-    url(r'^about/?$', 'app.views.about'),
-    url(r'^methods/?$', 'app.views.methods'),
-    url(r'^ethno/overview/?$', 'app.views.ethno_overview'),
-    url(r'^ethno/?$', 'app.views.ethno_overview'),
-    url(r'^ethno/results/?$', 'app.views.ethno_results'),
-    url(r'^hist/overview/?$', 'app.views.hist_overview'),
-    url(r'^hist/?$', 'app.views.hist_overview'),
-    url(r'^hist/results/?$', 'app.views.hist_results'),
-    url(r'^hist/interview/(?P<interview_id>\d+)/?$', 'app.views.interview'),
-    url(r'^faq/?$', 'app.views.faq'),
-    url(r'^docs/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.PDF_ROOT,}),
+    url(r'^about/?$', views.about),
+    url(r'^methods/?$', views.methods),
+    url(r'^ethno/overview/?$', views.ethno_overview),
+    url(r'^ethno/?$', views.ethno_overview),
+    url(r'^ethno/results/?$', views.ethno_results),
+    url(r'^hist/overview/?$', views.hist_overview),
+    url(r'^hist/?$', views.hist_overview),
+    url(r'^hist/results/?$', views.hist_results),
+    url(r'^hist/interview/(?P<interview_id>\d+)/?$', views.interview),
+    url(r'^faq/?$', views.faq),
+    url(r'^docs/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.PDF_ROOT,}),
     # web service
-    url(r'^ws/1.0/info$', 'app.views.ws_info'),
+    url(r'^ws/1.0/info$', views.ws_info),
     # Help content
-    url(r'^help/(?P<content_id>[-\w\d]+)/?$', 'app.views.show_help'),
+    url(r'^help/(?P<content_id>[-\w\d]+)/?$', views.show_help),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tinymce/', include('tinymce.urls')),
-)
+]
 
 wspatterns = i18n_patterns('',
     # web service
-    url(r'^ws/1.0/?$', 'app.views.ws_metadata'),
-    url(r'^ws/1.0/sp/?$', 'app.views.search_species', {'ws': True,}),
-    url(r'^ws/1.0/sp/(?P<species_id>\d+)/?$', 'app.views.show_species', {'ws': True,}),
+    url(r'^ws/1.0/?$', views.ws_metadata),
+    url(r'^ws/1.0/sp/?$', views.search_species, {'ws': True,}),
+    url(r'^ws/1.0/sp/(?P<species_id>\d+)/?$', views.show_species, {'ws': True,}),
 )
 
 urlpatterns += wspatterns
